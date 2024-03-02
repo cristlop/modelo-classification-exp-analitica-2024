abc7f2068fabba19662ccd00b8ec1df8b7c3ce47
cristlop <144555624+cristlop@users.noreply.github.com>
2024-03-02 09:25:34 -0500

    Update train.py

--- src/model/train.py
+++ src/model/train.py
@@ -67,7 +67,7 @@ wandb.sklearn.plot_feature_importances(model, feature_names=feature_names)
 y_pred = (y_probas > 0.5).astype(int)
 
 # Utilizar wandb.sklearn.plot_roc para la curva ROC
-wandb.sklearn.plot_roc(y_test, y_probas, labels)
+wandb.sklearn.plot_roc(y_test_binary, y_probas)
 
 # Visualizar evaluación del clasificador
 wandb.sklearn.plot_classifier(model,
