40d47e42a4314b936cbc9cc743dd2b4641e4f474
cristlop <144555624+cristlop@users.noreply.github.com>
2024-03-02 10:19:13 -0500

    Update train.py

--- src/model/train.py
+++ src/model/train.py
@@ -3,11 +3,8 @@ from sklearn.datasets import load_breast_cancer
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 import wandb
-from sklearn.metrics import roc_curve, auc, average_precision_score
-from sklearn.metrics import precision_recall_curve
+from sklearn.metrics import roc_curve, auc, average_precision_score, precision_recall_curve, plot_roc_curve, plot_confusion_matrix
 import matplotlib.pyplot as plt
-from sklearn.metrics import plot_roc_curve, plot_confusion_matrix
-
 
 # Cargar datos
 wbcd = load_breast_cancer()
@@ -50,6 +47,12 @@ roc_auc = auc(fpr, tpr)
 print("Curva ROC - fpr:", fpr)
 print("Curva ROC - tpr:", tpr)
 
+# Plotear la curva ROC directamente
+plt.figure()
+roc_curve_chart = plot_roc_curve(model, X_test, y_test_binary)
+plt.title('Curva ROC')
+plt.show()
+
 # Guardar los datos de la curva ROC
 roc_curve_data = {"fpr": fpr.tolist(), "tpr": tpr.tolist(), "roc_auc": roc_auc}
 
@@ -78,25 +81,11 @@ wandb.sklearn.plot_feature_importances(model, feature_names=feature_names)
 y_pred = (y_probas > 0.5).astype(int)
 
 # Visualizar evaluación del clasificador
-wandb.sklearn.plot_calibration_curve(model, X_test, y_test)
-
-# Plot de la curva ROC usando scikit-learn
-fig, ax = plt.subplots()
-roc_display = plot_roc_curve(model, X_test, y_test, ax=ax)
-roc_chart = roc_display.ax_
-roc_chart.set_title("Curva ROC")
-
-# Log de la curva ROC en W&B
-wandb.log({"Curva ROC": wandb.Image(roc_chart)})
-
-# Plot de la matriz de confusión
-fig, ax = plt.subplots()
-confusion_matrix_display = plot_confusion_matrix(model, X_test, y_test, ax=ax, display_labels=labels)
-confusion_matrix_chart = confusion_matrix_display.ax_
-confusion_matrix_chart.set_title("Matriz de Confusión")
-
-# Log de la matriz de confusión en W&B
-wandb.log({"Matriz de Confusión": wandb.Image(confusion_matrix_chart)})
+plt.figure()
+confusion_matrix_chart = plot_confusion_matrix(model, X_test, y_test_binary)
+plt.title('Matriz de confusión')
+plt.show()
+wandb.log({"Confusion_Matrix": confusion_matrix_chart})
 
 # Finalizar corrida en W&B
 wandb.finish()
