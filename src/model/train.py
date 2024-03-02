8bcf68c48eb41714890c2cdc0c3c8df24f84adf4
cristlop <144555624+cristlop@users.noreply.github.com>
2024-03-01 21:09:01 -0500

    Update train.py

--- src/model/train.py
+++ src/model/train.py
@@ -32,12 +32,13 @@ wandb.sklearn.plot_class_proportions(y_train, y_test, labels)
 wandb.sklearn.plot_learning_curve(model, X_train, y_train)
 
 # Calcular la curva ROC
-fpr, tpr, thresholds = roc_curve(y_test, y_probas)
+fpr, tpr, _ = roc_curve(y_test, y_probas)
 roc_auc = auc(fpr, tpr)
 
 # Registrar la curva ROC y el área bajo la curva en Weights & Biases
-roc_curve_dict = {"fpr": fpr.tolist(), "tpr": tpr.tolist(), "thresholds": thresholds.tolist()}
-wandb.log({"roc_auc": roc_auc, "roc_curve": roc_curve_dict})
+roc_curve_plot = wandb.plots.roc_curve(y_test, y_probas, labels=[str(i) for i in range(len(np.unique(y_test)))])
+wandb.log({"roc_auc": roc_auc, "roc_curve": roc_curve_plot})
+
 
 # Calcular la curva Precisión-Recall usando scikit-learn
 precision, recall, thresholds_pr = precision_recall_curve(y_test, y_probas)
